1773a966e4302f5a3498927db8ffed1efe81476e
kwbl <kwb201@gmail.com>
2021-12-27 14:20:15 -0500

    initial commit, test end points

---
+++ src/main/java/com/explore/springelastic/api/server/DefaultRestApi.java
@@ -0,0 +1,28 @@
+package com.explore.springelastic.api.server;
+
+import java.time.LocalTime;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class DefaultRestApi {
+	
+	@GetMapping(value = "/welcome")
+	public String welcome() {
+		System.out.println(StringUtils.join("Hello", " this is Spring Boot"));
+		return "Welcome to Spring Boot";
+	}
+	
+	@GetMapping(value = "/time")
+	public String time() {
+		return LocalTime.now().toString();
+	
+		 
+	}
+
+
+}
